1288c3f5fc2c39c28cd5a9c4a9a4d6d93d774f89
Milind Agarwal <agarwalmilind77@gmail.com>
2026-04-04 12:19:25 +0530

    Intial commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL)
+    console.log('connected to database');
+  } catch (err){
+    console.error(err);
+  }
+};
+
+module.exports = connectDB;
